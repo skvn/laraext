62b2c3367d9a78b7e728fc9d949e60f3ea35418f
serg <ku1yamin_sergey@mail.ru>
2015-03-03 10:58:55 +0300

    -- logger

---
+++ src/Log/LoggerTrait.php
@@ -0,0 +1,37 @@
+<?php namespace Laraext\Log;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+
+trait LoggerTrait {
+
+    protected $logger = null;
+
+
+    protected function getLogger()
+    {
+        if (is_null($this->logger))
+        {
+            $this->createLogger();
+        }
+        return $this->logger;
+    }
+
+    protected function createLogger()
+    {
+        $this->logger = new Logger(propery_exists($this, "__log_title") ? $this->__log_title : "");
+        $file = property_exists($this, "__log_file") ? storage_path("logs/" . $this->__log_file) : storage_path("logs/common.log");
+        if (strpos($file, '%') !== false)
+        {
+            $file = str_replace("%d", date("Ymd"), $file);
+        }
+        if (!file_exists(dirname($file)))
+        {
+            mkdir(dirname($file));
+        }
+        $this->logger->pushHandler(new StreamHandler($file), Logger::INFO);
+    }
+
+
+}
